c54718e81c765bd19b5b5defa1600b0fba122ce7
zuimeidetaiyang1116 <61719611+zuimeidetaiyang1116@users.noreply.github.com>
2021-10-01 15:52:47 +0800

    添加了于分析豆瓣Flask
    应用python爬虫，分析Flask框架搭建web，Echarts进行可视化

---
+++ Flask_douban/app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/index')
+def index():
+    # return render_template("index.html")
+    return hello_world()
+
+
+@app.route('/movie')
+def movie():
+    datalist = []
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = "select * from movie250"
+    data = cur.execute(sql)
+    for item in data:
+        datalist.append(item)
+    cur.close()
+    con.close()
+    return render_template("movie.html", movies=datalist)
+
+
+@app.route('/score')
+def score():
+    score = []  # 评分
+    num = []  # 每个电影的统计的电影数量
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = "select score, count(score) from movie250 group by score"
+    data = cur.execute(sql)
+    for item in data:
+        score.append(str(item[0]))
+        num.append(item[1])
+    cur.close()
+    con.close()
+    return render_template("score.html", score=score, num=num)
+
+
+@app.route('/word')
+def word():
+    return render_template("word.html")
+
+
+@app.route('/team')
+def team():
+    return render_template("team.html")
+
+
+if __name__ == '__main__':
+    app.run()
